9dbd2acf4347da7a8feaa72047fa4c711e1f1296
branningbasir <branningbasir@gmail.com>
2025-04-09 15:45:39 +0000

    Add 27.cpp

---
+++ src/27.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    cout << "You are " << (age == 18) ? "a teenager" : "an adult"; // true is 'a', false is 'an'
+    return 0;
+}
