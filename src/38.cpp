2e96ecbe9cb2b0afff067d02ea38ab4945d9733b
branningbasir <branningbasir@gmail.com>
2025-05-24 08:37:54 +0000

    Add 38.cpp

---
+++ src/38.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "Positive number" << endl;
+    } else {
+        cout << "Negative or zero" << endl;
+    }
+}
